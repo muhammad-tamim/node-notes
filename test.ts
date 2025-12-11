57293952c12333f1d1f746e0109b96c59b2df7e3
muhammad-tamim <contact2tamim@gmail.com>
2025-12-11 23:31:10 +0600

    add(Part 5: Zod): add Zod Basic

---
+++ test.ts
@@ -0,0 +1,14 @@
+import * as z from "zod";
+
+const User = z.object({
+    name: z.string(),
+});
+
+// some untrusted data...
+const input = { /* stuff */ };
+
+// the parsed result is validated and type safe!
+const data = User.parse(input);
+
+// so you can use it with confidence :)
+console.log(data.name);
